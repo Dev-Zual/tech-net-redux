fdf53e4928afb38e15118fc67de58797a41945ad
Zual Rana <freelancerzual@gmail.com>
2023-08-24 17:00:22 +0600

    [DONE] setup redux toolkit

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function home() {
   return <div>home</div>;
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import NotFound from "../page/NotFound";
+import Login from "../page/Login";
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
   {
-    path: "/home",
-    element: <Home,
+    path: "/login",
+    element: <Login />,
   },
   {
-    path: "/",
-    element: <App />,
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
