80ab029e687a0f34c7dfb0b6ac55eb25ffd4f861
Zual Rana <freelancerzual@gmail.com>
2023-08-27 13:01:43 +0600

    [DONE] created redux custom logger middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 function App() {
+  const count = useAppSelector((state) => state.count.value);
+  const dispatch = useAppDispatch();
   return (
     <>
-      <h1 className="bg-red-500">tech net</h1>
+      <div>
+        <div className="flex gap-3 mt-2">
+          <button
+            onClick={() => dispatch(decrement())}
+            className="border-red-400 px-3 py-2 border-2 rounded"
+          >
+            decrement
+          </button>
+          <div>{count}</div>
+          <button
+            onClick={() => dispatch(increment())}
+            className="border-green-400 px-3 py-2 border-2 rounded"
+          >
+            increment
+          </button>
+        </div>
+      </div>
     </>
   );
 }
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./features/counter/counterSlice";
+import logger from "./middlewares/logger";
 
 export const store = configureStore({
   reducer: {
     count: counterSlice,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
